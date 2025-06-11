e2676b8f729224b72c41f51dc3533ab664a98e84
QuocHao <quochao170402@gmail.com>
2025-06-12 06:37:02 +0700

    chorn: config tanstackquery

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,24 +1,44 @@
 import MetricCard from "@/components/Cards/MetricCard";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
 import { View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+const queryClient = new QueryClient();
+
 export default function HomeScreen() {
+  const url = "https://dummyjson.com/todos";
+  const { data } = useQuery({
+    queryKey: ["todos"],
+    queryFn: async () => {
+      const response = await fetch(url);
+      if (!response.ok) throw new Error("Network response was not ok");
+      return response.json();
+    },
+  });
+
+  console.log("Api data: ", data);
   return (
-    <SafeAreaView>
-      <View
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between",
-          flexWrap: "wrap",
-          gap: 12,
-          padding: 12,
-        }}
-      >
-        <MetricCard title="Today" value="22" description="Today" />
-        <MetricCard title="Month" value="440" description="This month" />
-        <MetricCard title="Year" value="7k" description="Year to date" />
-      </View>
-    </SafeAreaView>
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaView>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            flexWrap: "wrap",
+            gap: 12,
+            padding: 12,
+          }}
+        >
+          <MetricCard title="Today" value="22" description="Today" />
+          <MetricCard title="Month" value="440" description="This month" />
+          <MetricCard title="Year" value="7k" description="Year to date" />
+        </View>
+      </SafeAreaView>
+    </QueryClientProvider>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,6 +9,9 @@ import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
 
 import { useColorScheme } from "@/hooks/useColorScheme";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -22,14 +25,16 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack initialRouteName="(tabs)">
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <Stack initialRouteName="(tabs)">
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
 
-        {/* <Stack.Screen name="index" options={{ headerShown: false }} /> */}
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+          {/* <Stack.Screen name="index" options={{ headerShown: false }} /> */}
+          <Stack.Screen name="+not-found" />
+        </Stack>
+        <StatusBar style="auto" />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
